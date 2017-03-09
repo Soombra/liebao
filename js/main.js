0a1e6deac90d3ea85d890a50ef30fe5aa5b4710d
Soombra <772850334@qq.com>
2017-03-09 17:00:07 +0800

    雏形

---
+++ js/main.js
@@ -0,0 +1,48 @@
+var pre=document.getElementById("pre"),
+		next=document.getElementById("next");
+	var banner=document.getElementById("banner");
+	var timer;
+	var bt_left=document.getElementById("bt_left");
+	var bt_right=document.getElementById("bt_right");
+
+window.onload=function(){
+	
+	pre.onclick=function(){
+		bannerChange();
+	}
+	next.onclick=function(){
+		bannerChange();
+	}
+	bt_left.onclick=function(){
+		changeOrange();
+	}
+	bt_right.onclick=function(){
+		changeBlue();
+	}
+	timer=setTimeout(bannerChange,3000);
+
+}
+
+function bannerChange(){
+	if(banner.className=="banner orange"){
+		changeBlue();
+		return;
+	}
+	changeOrange();
+}
+
+function changeOrange(){
+	banner.className="banner orange";
+	bt_left.className="span_active";
+	bt_right.className="";
+	clearTimeout(timer);
+	timer=setTimeout(bannerChange,3000);
+}
+
+function changeBlue(){
+	banner.className="banner blue";
+	bt_left.className="";
+	bt_right.className="span_active";
+	clearTimeout(timer);
+	timer=setTimeout(bannerChange,3000);
+}
